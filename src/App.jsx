e2407dea4db07dd092dc097086e996a32adec6bb
mivCalik <merve.n.clk@gmail.com>
2026-06-30 23:49:10 +0300

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-  
-    </>
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+
+        <Route path='/' element={<Home/>} />
+        <Route path='/auth' element = {<Auth/>} />
+        <Route path='/checkout' element= {<Checkout/>} />
+
+      </Routes>
+    </div>
   )
 }
 
